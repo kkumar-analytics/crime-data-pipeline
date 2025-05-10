2fef82ac5da5ea5563a59a405e025e1e84779bc6
Kuldeep Kumar <kuldeep.kumar@gatech.edu>
2025-05-10 16:29:31 -0400

    Add age_range column for reporting purpose

--- airflow/dbt/lapd_crime_project/models/marts/reporting/victim_demographics_summary.sql
+++ airflow/dbt/lapd_crime_project/models/marts/reporting/victim_demographics_summary.sql
@@ -5,15 +5,22 @@
 ) }}
 
 SELECT
-    dv.age AS victim_age,
+    CASE
+        WHEN dv.age BETWEEN 0 AND 10 THEN '0-10'
+        WHEN dv.age BETWEEN 11 AND 20 THEN '11-20'
+        WHEN dv.age BETWEEN 21 AND 30 THEN '21-30'
+        WHEN dv.age BETWEEN 31 AND 40 THEN '31-40'
+        WHEN dv.age BETWEEN 41 AND 50 THEN '41-50'
+        WHEN dv.age BETWEEN 51 AND 60 THEN '51-60'
+        WHEN dv.age BETWEEN 61 AND 70 THEN '61-70'
+        WHEN dv.age BETWEEN 71 AND 80 THEN '71-80'
+        WHEN dv.age BETWEEN 81 AND 90 THEN '81-90'
+        WHEN dv.age > 90 THEN '91+'
+        ELSE 'Unknown'
+    END AS age_range,
     dv.sex AS victim_sex,
     dv.descent AS victim_descent,
-    COUNT(*) AS incident_count,
-    SUM(COUNT(*)) OVER (PARTITION BY dv.age, dv.sex) AS total_by_age_sex,
-    ROUND(
-        (COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (PARTITION BY dv.age, dv.sex),
-        2
-    ) AS descent_percentage
+    COUNT(*) AS incident_count
 FROM
     {{ ref('fct_crime_events') }} AS fce
     JOIN {{ ref('dim_victim') }} AS dv ON fce.victims_id = dv.id
